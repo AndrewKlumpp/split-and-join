7826861a74cf21180e265324f87c6c74b9b57189
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 11:55:59 -0700

    initial commit

---
+++ shortestWord.js
@@ -0,0 +1,7 @@
+// Your code here
+
+
+
+console.log(shortestWord('what a wonderful life'));     // 'a'
+console.log(shortestWord('the quick brown fox jumps')); // 'fox'
+console.log(shortestWord('do what you enjoy'));         // 'do'
