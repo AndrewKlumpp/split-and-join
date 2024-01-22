2626d4e13ddd814b208d7e7dda7b8dc3bfb2d000
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 13:34:42 -0700

    initial commit

---
+++ hipsterfy.js
@@ -0,0 +1,8 @@
+function hipsterfy(sentence) {
+
+};
+
+
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
