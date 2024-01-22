f66d89d9baf9fc9c859720d5f40a3b6a72ab7f1f
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 12:00:56 -0700

    finished shortestWord

--- shortestWord.js
+++ shortestWord.js
@@ -1,6 +1,16 @@
-// Your code here
+function shortestWord(sentence) {
+  let words = sentence.split(' ');
+  let shortest = words[0];
 
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (word.length <= shortest.length) {
+      shortest = word;
+    }
+  }
 
+  return shortest;
+};
 
 console.log(shortestWord('what a wonderful life'));     // 'a'
 console.log(shortestWord('the quick brown fox jumps')); // 'fox'
