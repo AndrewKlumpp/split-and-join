2686e74ce5831838ae4708624c4c9bdd3e4c46d2
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 12:31:27 -0700

    initial commit

---
+++ abbreviateWords.js
@@ -0,0 +1,7 @@
+function abbreviateWords(sentence) {
+
+}
+
+
+console.log(abbreviateWords('what a wonderful place to live')); // what a wndrfl plc to live
+console.log(abbreviateWords('she sends an excellent message ')); // she snds an xcllnt mssg
