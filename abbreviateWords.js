446954e7d78e22739ad4b465c9bffa631466ce25
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 12:51:38 -0700

    finished abbreviateWords

--- abbreviateWords.js
+++ abbreviateWords.js
@@ -1,7 +1,29 @@
 function abbreviateWords(sentence) {
+  let words = sentence.split(' ');
+  let newWords = [];
 
-}
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (word.length > 4) {
+      newWords.push(removeVowels(word));
+    } else {
+      newWords.push(word);
+    }
+  }
+  return newWords.join(' ');
+};
 
+function removeVowels(word) {
+  let abbreviated = '';
+  let vowels = 'aeiou';
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    if (!vowels.includes(letter)) {
+      abbreviated += letter;
+    }
+  }
+  return abbreviated;
+};
 
 console.log(abbreviateWords('what a wonderful place to live')); // what a wndrfl plc to live
 console.log(abbreviateWords('she sends an excellent message ')); // she snds an xcllnt mssg
