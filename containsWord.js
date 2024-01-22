e95041bcd8c12db5109a312a9151011ee450aba0
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 12:18:46 -0700

    initial commit

---
+++ containsWord.js
@@ -0,0 +1,9 @@
+function containsWord(sentence, targetWord) {
+
+}
+
+
+console.log(containsWord('sounds like a plan', 'like')); // true
+console.log(containsWord('They are great', 'they')); // true
+console.log(containsWord('caterpillars are great animals', 'cat')); // false
+console.log(containsWord('Cast the net', 'internet')); // false
