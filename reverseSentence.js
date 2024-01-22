fbf0bcf5b2bc5d0d8d620eb4c5d098dfc1f65758
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 12:03:53 -0700

    initial commit

---
+++ reverseSentence.js
@@ -0,0 +1,7 @@
+function reverseSentence(sentence) {
+
+};
+
+
+console.log(reverseSentence('I am pretty hungry')); // 'hungry pretty am I'
+console.log(reverseSentence('follow the yellow brick road')); // 'road brick yellow the follow'
