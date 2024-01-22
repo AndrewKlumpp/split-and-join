1be8df01703176a6abdfdeeff1beb4ef1bb31f4b
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-22 11:32:43 -0700

    initial commit

---
+++ longestWord.js
@@ -0,0 +1,9 @@
+function longestWord(sentence) {
+
+};
+
+
+
+console.log(longestWord('where did everyone go')); // 'everyone'
+console.log(longestWord('prefer simplicity over complexity')); // 'simplicity'
+console.log(longestWord('')); // ''
