7875ef8f595f520b3121c8262304c05d29d0879d
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-23 07:22:49 -0700

    finished hipsterfy

--- hipsterfy.js
+++ hipsterfy.js
@@ -1,5 +1,24 @@
 function hipsterfy(sentence) {
+  let words = sentence.split(' ');
+  let hipstered = [];
 
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    hipstered.push(removeLastVowel(word));
+  }
+  return hipstered.join(' ');
+};
+
+function removeLastVowel(word) {
+
+  let vowels = 'aeiou';
+  for (let i = word.length - 1; i >= 0; i--) {
+    let letter = word[i];
+    if (vowels.includes(letter)) {
+      return word.slice(0, i) + word.slice(i + 1);
+    }
+  }
+  return word;
 };
 
 
