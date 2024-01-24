8efb3eff677ca32c62ca0d93e860e519d5614df9
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 12:41:44 -0700

    finished sameCharCollapse

--- sameCharCollapse.js
+++ sameCharCollapse.js
@@ -1,6 +1,21 @@
 function sameCharCollapse(str) {
-  // your code here
-}
+  let reducible = true;
+
+  while (reducible) {
+    let chars = str.split('');
+    reducible = false;
+
+    for (let i = 0; i < chars.length; i++) {
+      if (chars[i] == chars[i + 1]) {
+        chars[i] = '';
+        chars[i + 1] = '';
+        reducible = true;
+      }
+    }
+    str = chars.join('');
+  }
+  return str;
+};
 
 console.log(sameCharCollapse("zzzxaaxy"));  // "zy"
 // because zzzxaaxy -> zxaaxy -> zxxy -> zy
