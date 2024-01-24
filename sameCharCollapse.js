e058348938a2a10c73cf1e41874ec99c642238ed
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 07:59:41 -0700

    initial commit

---
+++ sameCharCollapse.js
@@ -0,0 +1,8 @@
+function sameCharCollapse(str) {
+  // your code here
+}
+
+console.log(sameCharCollapse("zzzxaaxy"));  // "zy"
+// because zzzxaaxy -> zxaaxy -> zxxy -> zy
+console.log(sameCharCollapse("uqrssrqvtt")); // "uv"
+// because uqrssrqvtt -> uqrrqvtt -> uqqvtt -> uvtt -> uv
