d646fb493386dfb451afc3992faf49450f361815
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 07:41:10 -0700

    finished consonantCancel

--- consonantCancel.js
+++ consonantCancel.js
@@ -1,6 +1,24 @@
 function consonantCancel(sentence) {
-  // your code here
-}
+  let words = sentence.split(' ');
+  let newWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    newWords.push(firstVowel(word));
+  }
+  return newWords.join(' ');
+};
+
+function firstVowel(word) {
+  let vowels = 'aeiou';
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    if (vowels.includes(letter)) {
+      return word.slice(i);
+    }
+  }
+  return word;
+};
 
 console.log(consonantCancel("down the rabbit hole")); // "own e abbit ole"
 console.log(consonantCancel("writing code is challenging")); // "iting ode is allenging"
