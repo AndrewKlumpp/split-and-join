a71c80f6cbe3616f39aca7914dc21de2af145c92
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 07:21:18 -0700

    initial commit

---
+++ consonantCancel.js
@@ -0,0 +1,6 @@
+function consonantCancel(sentence) {
+  // your code here
+}
+
+console.log(consonantCancel("down the rabbit hole")); // "own e abbit ole"
+console.log(consonantCancel("writing code is challenging")); // "iting ode is allenging"
